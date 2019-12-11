82991dd196c5c2328636a798fe201c4d37c5928c
Vojta Fišer <fiservo32@fjfi.cvut.cz>
2019-12-11 12:06:54 +0100

    Dalsi uprava hotova

--- statistika/clenstvi_histogram.py
+++ statistika/clenstvi_histogram.py
@@ -17,20 +17,25 @@ end = 20
 bins = np.linspace(beg-0.5, end-0.5, num=end-beg+1)
 
 
-fig, (ax_a, ax_h) = plt.subplots(2, 1, figsize=(10, 6))
+plt.rcParams['font.size'] = 12
+fig, (ax_a, ax_h) = plt.subplots(2, 1, figsize=(8, 5))
 
+
+yticks = np.linspace(0, 12, num= 7)
 
 ax_a.hist(mem_a, bins=bins, rwidth = 0.9)
 ax_a.set_xticks(bins+0.5)
 ax_a.set_ylabel("Anpetu")
 ax_a.grid(axis = "y")
 ax_a.set_axisbelow(True)
+ax_a.set_yticks(yticks)
 
 ax_h.hist(mem_h, bins=bins, rwidth = 0.9)
 ax_h.set_xticks(bins+0.5)
 ax_h.set_ylabel("Hanyetu")
 ax_h.grid(axis = "y")
 ax_h.set_axisbelow(True)
+ax_h.set_yticks(yticks)
 
 
 ax_h.set_xlabel("Roky v oddíle")
--- statistika/vek_histogram.py
+++ statistika/vek_histogram.py
@@ -14,20 +14,26 @@ end = 25
 
 bins = np.linspace(beg-0.5, end-0.5, num=end-beg+1)
 
+plt.rcParams['font.size'] = 12
 
-fig, (ax_a, ax_h) = plt.subplots(2, 1, figsize=(10, 6))
+
+yticks = np.linspace(0, 10, num= 6)
+
+fig, (ax_a, ax_h) = plt.subplots(2, 1, figsize=(8, 5))
 
 ax_a.hist(age_a, bins=bins, rwidth = 0.9)
 ax_a.set_xticks(bins+0.5)
 ax_a.set_ylabel("Anpetu")
 ax_a.grid(axis = "y")
 ax_a.set_axisbelow(True)
+ax_a.set_yticks(yticks)
 
 ax_h.hist(age_h, bins=bins, rwidth = 0.9)
 ax_h.set_xticks(bins+0.5)
 ax_h.set_ylabel("Hanyetu")
 ax_h.grid(axis = "y")
 ax_h.set_axisbelow(True)
+ax_h.set_yticks(yticks)
 
 
 ax_h.set_xlabel("Věk")
